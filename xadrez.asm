30878b9d7673c3233a80ccf1fdeec272565d4149
Marina <marinamcsouza@gmail.com>
2025-01-12 21:03:24 -0300

    feat: adiciona loop principal de linhas

--- xadrez.asm
+++ xadrez.asm
@@ -6,3 +6,10 @@ main:
 	
 	la $a3, $board		# aponta para o inicio da lista de linhas (array 2d, matriz)
 	subi $a3, $a3, 4	# tira 4 para ser adicionado a cada passagem de laco
+
+loopline:
+    addi $a0, $a0, 1    # incrementa o contador de linha
+    bgt $a0, 7, end     # finaliza o programa ao completar as linhas
+    li $a1, 0           # reseta o contador de colunas
+    addi $a3, $a3, 4    # ajusta o ponteiro para a próxima linha
+    ld $t1, ($a3)       # carrega o endereço da linha atual
